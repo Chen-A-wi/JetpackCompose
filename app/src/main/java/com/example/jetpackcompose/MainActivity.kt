c6cef4b89fcf3339a319df63da5ca8277faafd1c
Ben <benchen@starlux-airlines.com>
2020-01-12 22:18:39 +0800

    [feat] Add a picture

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.core.setContent
-import androidx.ui.layout.Column
-import androidx.ui.layout.Spacing
+import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
+import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +27,21 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewStory() {
-    Column(modifier = Spacing(16.dp)) {
+    val image = +imageResource(R.drawable.head)
+
+    Column(
+        modifier = Spacing(16.dp)
+    ) {
+        Container(modifier = Height(180.dp) wraps Expanded) {
+            DrawImage(image)
+        }
+
+        HeightSpacer(16.dp)
+
         Text(text = "A long time ago and far away there lived a King and a Queen.")
         Text(text = "They were very happy, for their first child, a girl, had been born.")
     }
+
 }
 
 @Preview
