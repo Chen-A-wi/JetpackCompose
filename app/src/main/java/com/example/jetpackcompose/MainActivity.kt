31217ef83fd247da12158b703141e280d7c51f0d
Ben <benchen@starlux-airlines.com>
2020-01-12 20:46:41 +0800

    [feat] Add style settings to the column

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.layout.Column
+import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
 import kotlin.random.Random
@@ -25,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun NewStory() {
-    Column {
+    Column(modifier = Spacing(16.dp)) {
         Text(text = "A long time ago and far away there lived a King and a Queen.")
         Text(text = "They were very happy, for their first child, a girl, had been born.")
     }
